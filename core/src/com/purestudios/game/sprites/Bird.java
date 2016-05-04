224a92987830846e90472a6722d97e3a400f1043
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 22:57:25 +1000

    Bird Jumps on Touch, BG added to Gameplay, Y axis constraint - End of Video 7

--- core/src/com/purestudios/game/sprites/Bird.java
+++ core/src/com/purestudios/game/sprites/Bird.java
@@ -20,10 +20,17 @@ public class Bird {
   }
   
   public void update(float dt) {
-    velocity.add(0, GRAVITY, 0);
-    velocity.scl(dt);
+    if (position.y > 0) {
+      velocity.add(0, GRAVITY, 0);
+    }
     
+    velocity.scl(dt);
     position.add(0, velocity.y, 0);
+    
+    if (position.y < 0) {
+      position.y = 0;
+    }
+    
     velocity.scl(1/dt);
   }
 
@@ -35,4 +42,7 @@ public class Bird {
     return texture;
   }
   
+  public void jump() {
+    velocity.y = 250;
+  }
 }
--- core/src/com/purestudios/game/states/PlayState.java
+++ core/src/com/purestudios/game/states/PlayState.java
@@ -1,5 +1,7 @@
 package com.purestudios.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.purestudios.game.FlappyDemo;
 import com.purestudios.game.sprites.Bird;
@@ -7,16 +9,20 @@ import com.purestudios.game.sprites.Bird;
 public class PlayState extends State {
   
   private Bird bird;
+  private Texture background;
   
   public PlayState(GameStateManager gsm) {
     super(gsm);
     bird = new Bird(50, 300);
     cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+    background = new Texture("bg.png");
   }
   
   @Override
   public void handleInput() {
-    // TODO Auto-generated method stub
+    if (Gdx.input.justTouched()) {
+      bird.jump();
+    }
   }
   
   @Override
@@ -31,6 +37,7 @@ public class PlayState extends State {
     sb.setProjectionMatrix(cam.combined);
     
     sb.begin();
+    sb.draw(background, cam.position.x - (cam.viewportWidth / 2), 0);
     sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
     sb.end();
   }
