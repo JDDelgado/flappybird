e9e9d43713213385ac7a71309c513257d952046f
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 22:10:29 +1000

    First Commit - End of Video 3

---
+++ core/src/com/purestudios/game/states/MenuState.java
@@ -0,0 +1,25 @@
+package com.purestudios.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+  
+  public MenuState(GameStateManager gsm) {
+    super(gsm);
+  }
+  
+  @Override
+  public void handleInput() {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void update(float dt) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void render(SpriteBatch sb) {
+    // TODO Auto-generated method stub
+  }
+}
