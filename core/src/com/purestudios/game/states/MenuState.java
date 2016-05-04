e21a790e6e8cb157cf3ae095dac2b684c282b7e6
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 23:40:42 +1000

    Disposing Textures to avoid Memory Leaks

--- core/src/com/purestudios/game/states/MenuState.java
+++ core/src/com/purestudios/game/states/MenuState.java
@@ -21,7 +21,6 @@ public class MenuState extends State {
   public void handleInput() {
     if (Gdx.input.justTouched()) {
       gsm.set(new PlayState(gsm));
-      dispose();
     }
   }
 
@@ -44,5 +43,7 @@ public class MenuState extends State {
   public void dispose() {
     background.dispose();
     playButton.dispose();
+    
+    System.out.println("Menu State Dispose");
   }
 }
