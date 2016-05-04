ee924fb0cd0691ae51ad2ede9856b3a28c16cbd5
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 23:07:12 +1000

    Tubes added into the game randomly generated

---
+++ core/src/com/purestudios/game/sprites/Tube.java
@@ -0,0 +1,43 @@
+package com.purestudios.game.sprites;
+
+import java.util.Random;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class Tube {
+  
+  private static final int FLUCTUATION = 130;
+  private static final int TUBE_GAP = 100;
+  private static final int LOWEST_OPENNING_TUBE = 120;
+  
+  private Texture topTube, bottomTube;
+  private Vector2 posTopTube, posBotTube;
+  private Random rand;
+  
+  public Tube(float x) {
+    topTube = new Texture("toptube.png");
+    bottomTube = new Texture("bottomtube.png");
+    rand = new Random();
+    
+    posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENNING_TUBE);
+    posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+  }
+  
+  public Texture getTopTube() {
+    return topTube;
+  }
+  
+  public Texture getBottomTube() {
+    return bottomTube;
+  }
+  
+  public Vector2 getPosTopTube() {
+    return posTopTube;
+  }
+  
+  public Vector2 getPosBotTube() {
+    return posBotTube;
+  }
+  
+}
--- core/src/com/purestudios/game/states/PlayState.java
+++ core/src/com/purestudios/game/states/PlayState.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.purestudios.game.FlappyDemo;
 import com.purestudios.game.sprites.Bird;
+import com.purestudios.game.sprites.Tube;
 
 public class PlayState extends State {
   
   private Bird bird;
   private Texture background;
+  private Tube tube;
   
   public PlayState(GameStateManager gsm) {
     super(gsm);
     bird = new Bird(50, 300);
     cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
     background = new Texture("bg.png");
+    tube = new Tube(100);
   }
   
   @Override
@@ -39,6 +42,8 @@ public class PlayState extends State {
     sb.begin();
     sb.draw(background, cam.position.x - (cam.viewportWidth / 2), 0);
     sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+    sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+    sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
     sb.end();
   }
   
