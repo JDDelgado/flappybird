7285b9fe982590320bc67eaa14f1e31fd684812d
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 23:31:24 +1000

    Add Rectangles for Collisions

--- core/src/com/purestudios/game/sprites/Tube.java
+++ core/src/com/purestudios/game/sprites/Tube.java
@@ -3,6 +3,7 @@ package com.purestudios.game.sprites;
 import java.util.Random;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Tube {
@@ -16,6 +17,8 @@ public class Tube {
   private Vector2 posTopTube, posBotTube;
   private Random rand;
   
+  private Rectangle boundsTop, boundsBot;
+  
   public Tube(float x) {
     topTube = new Texture("toptube.png");
     bottomTube = new Texture("bottomtube.png");
@@ -23,6 +26,9 @@ public class Tube {
     
     posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENNING_TUBE);
     posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    
+    boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+    boundsBot = new Rectangle(posBotTube.x, posBotTube.y, bottomTube.getWidth(), bottomTube.getHeight());
   }
   
   public Texture getTopTube() {
@@ -44,6 +50,12 @@ public class Tube {
   public void reposition(float x) {
     posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENNING_TUBE);
     posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    
+    boundsTop.setPosition(posTopTube.x, posTopTube.y);
+    boundsBot.setPosition(posBotTube.x, posBotTube.y);
   }
   
+  public boolean collides(Rectangle player) {
+    return player.overlaps(boundsTop) || player.overlaps(boundsBot);
+  }
 }
