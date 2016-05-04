af4d4b671716ee7b1d8e0f6147ecbc4cdb864d0c
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-04 22:25:28 +1000

    Assets Added - End of Video 4

--- core/src/com/purestudios/game/states/MenuState.java
+++ core/src/com/purestudios/game/states/MenuState.java
@@ -1,11 +1,19 @@
 package com.purestudios.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.purestudios.game.FlappyDemo;
 
 public class MenuState extends State {
   
+  private Texture background;
+  private Texture playButton;
+  
   public MenuState(GameStateManager gsm) {
     super(gsm);
+    
+    background = new Texture("bg.png");
+    playButton = new Texture("playbtn.png");
   }
   
   @Override
@@ -20,6 +28,12 @@ public class MenuState extends State {
 
   @Override
   public void render(SpriteBatch sb) {
-    // TODO Auto-generated method stub
+    int playBtnWidth = (FlappyDemo.WIDTH / 2) - (playButton.getWidth() / 2);
+    int playBtnHeight = (FlappyDemo.HEIGHT / 2) - (playButton.getHeight() / 2);
+    
+    sb.begin();
+    sb.draw(background, 0,0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+    sb.draw(playButton, playBtnWidth, playBtnHeight);
+    sb.end();
   }
 }
