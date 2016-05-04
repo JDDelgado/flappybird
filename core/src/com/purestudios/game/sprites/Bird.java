aa2704ac268abef7f56f6989f773314e9e62ec66
Juan Diego Delgado Vargas <jddelgadov@gmail.com>
2016-05-05 00:14:10 +1000

    Animation of Bird

--- core/src/com/purestudios/game/sprites/Bird.java
+++ core/src/com/purestudios/game/sprites/Bird.java
@@ -1,6 +1,7 @@
 package com.purestudios.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
@@ -12,18 +13,24 @@ public class Bird {
   private Vector3 position;
   private Vector3 velocity;
   
-  private Texture texture;
   private Rectangle bounds;
   
+  private Texture texture;
+  private Animation birdAnimation;
   
   public Bird(int x, int y) {
     position = new Vector3(x, y, 0);
     velocity = new Vector3(0,0,0);
-    texture = new Texture("bird.png");
-    bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+    
+    texture = new Texture("birdanimation.png");
+    birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+    
+    bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
   }
   
   public void update(float dt) {
+    birdAnimation.update(dt);
+    
     if (position.y > 0) {
       velocity.add(0, GRAVITY, 0);
     }
@@ -44,8 +51,8 @@ public class Bird {
     return position;
   }
 
-  public Texture getTexture() {
-    return texture;
+  public TextureRegion getTexture() {
+    return birdAnimation.getFrame();
   }
   
   public void jump() {
